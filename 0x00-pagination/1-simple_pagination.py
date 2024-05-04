1806152817922b4aac2e862716016b8d34025e31
Ali Bayomy <alibayomy12@gmail.com>
2024-05-04 16:48:30 +0300

    adding the shebang

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/env python3
+#!/usr/bin/env python3
 import csv
 import math
 from typing import List
@@ -16,7 +16,7 @@ def index_range(page, page_size):
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
+     
 
     def __init__(self):
         self.__dataset = None
