34a04b2e8952da1c6c92054b50e54ea07a5232f8
alibayomy <alibayomy12@gmail.com>
2024-05-04 13:24:27 +0000

    fixing pycodestyle and removing the testing statmetns

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,19 +3,12 @@
 get  a list for those particular pagination parameters
 """
 
+
 def index_range(page, page_size):
     """
-    return a tuple of size two containing 
-        a start index and an end index 
+    return a tuple of size two containing
+        a start index and an end index
     """
     if page == 1:
         return (0, page_size)
     return (page_size * (page - 1), page * page_size)
-
-res = index_range(1, 7)
-print(type(res))
-print(res)
-
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
