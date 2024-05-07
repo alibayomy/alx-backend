7f0e9fc3ba80965f75cd6efa5afce8cbba1e70a0
alibayomy <alibayomy12@gmail.com>
2024-05-07 10:49:25 +0000

    removing the equal sign in the last page check file num 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -57,7 +57,7 @@ class Server:
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
         page_size = len(data)
-        next_page = page + 1 if page <= total_pages else None
+        next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
         return {'page_size': page_size,
                 'page': page,
