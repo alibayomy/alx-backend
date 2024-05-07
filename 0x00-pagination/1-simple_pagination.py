975b2ba001db5fca0abacb3b096c8f53e23a5f12
alibayomy <alibayomy12@gmail.com>
2024-05-07 10:11:32 +0000

    handeling start index > len(data) case , in 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -44,4 +44,6 @@ class Server:
         assert page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
+        if start > len(data):
+            return []
         return data[start, end]
