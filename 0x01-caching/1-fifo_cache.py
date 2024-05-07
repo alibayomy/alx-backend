a3de9834f410f9ed7414351fb0ae724f7777cded
Ali Bayomy <alibayomy12@gmail.com>
2024-05-07 19:02:31 +0300

    adding FIFOCaache class that implemnting fifo algorithm in caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO Cache
+    Store caching by FIFO algorithm
+    """
+    def __init__(self):
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """
+        assgin item to the cache_dict, remove the
+        first in if full
+        """
+        discard_item = 'Z'
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            for k in self.cache_data.keys():
+                if k < discard_item:
+                    discard_item = k
+            del self.cache_data[discard_item]
+            print(f'DISCARD: {discard_item}')
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value of cache dict based on
+            the given key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
+
+
+            
+                
+
+
