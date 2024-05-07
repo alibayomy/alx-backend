065fac34acbf7f8634c60e2dc627937c7f327f3f
Ali Bayomy <alibayomy12@gmail.com>
2024-05-07 18:10:45 +0300

    adjusting pycodestyle for 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """BasicCache module
 """
-BaseCaching  = __import__('base_caching').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
     """ BasicCache defines:
@@ -23,24 +24,7 @@ class BasicCache(BaseCaching):
         """
         Return the value of cache dict based on
             the given key
-            """
+        """
         if key is None  or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
-    
-my_cache = BasicCache()
-my_cache.print_cache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-print(my_cache.get("D"))
-my_cache.print_cache()
-my_cache.put("D", "School")
-my_cache.put("E", "Battery")
-my_cache.put("A", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
