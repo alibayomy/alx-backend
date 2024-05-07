ad726f8515ce55a6e9462e0127006088b4c4f629
alibayomy <alibayomy12@gmail.com>
2024-05-07 10:14:13 +0000

    check the type of page and page_size in 1-simple_pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,8 +40,8 @@ class Server:
         """
         return the data based on the given page size
         """
-        assert page > 0
-        assert page_size > 0
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start > len(data):
