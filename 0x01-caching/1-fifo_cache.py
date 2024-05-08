346034071019e1796684011d7d81299fd03b379b
Ali Bayomy <alibayomy12@gmail.com>
2024-05-08 16:55:09 +0300

    adjusting pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,7 +16,7 @@ class FIFOCache(BaseCaching):
         assgin item to the cache_dict, remove the
         first in if full
         """
-        
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             keys_list = list(self.cache_data.keys())
             keys_list.sort()
@@ -33,15 +33,3 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
