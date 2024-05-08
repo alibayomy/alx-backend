555752ee40fcde6cd595b021c4625a1eb4c48480
Ali Bayomy <alibayomy12@gmail.com>
2024-05-08 16:54:03 +0300

    adjusting getting the first in algorithm

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,11 +16,11 @@ class FIFOCache(BaseCaching):
         assgin item to the cache_dict, remove the
         first in if full
         """
-        discard_item = 'Z'
+        
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            for k in self.cache_data.keys():
-                if k < discard_item:
-                    discard_item = k
+            keys_list = list(self.cache_data.keys())
+            keys_list.sort()
+            discard_item = keys_list[0]
             del self.cache_data[discard_item]
             print(f'DISCARD: {discard_item}')
         self.cache_data[key] = item
@@ -33,3 +33,15 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
+my_cache = FIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
