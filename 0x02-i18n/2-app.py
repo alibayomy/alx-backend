0527b3320d1ab370ce4f01d346ce25bd68906b4a
Ali Bayomy <alibayomy12@gmail.com>
2024-05-14 23:28:49 +0300

    pushing 2-app.py for testing

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Task 0: Basic Flask app
+'''
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ class that has a LANGUAGES class
+    attribute equal to ["en", "fr"]."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel()
+
+
+@babel.localeselector
+def get_locale():
+    """Retrieves the locale for a web page."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """Render the home page"""
+    return render_template('0-index.html',)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
