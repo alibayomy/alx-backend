4bfe45a22f22ae2da8e7ebdd4a067ac0c1c41089
Ali Bayomy <alibayomy12@gmail.com>
2024-05-14 23:24:09 +0300

    pushing 1-app.py and 1-index.html for testing

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,10 +5,17 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+class Config:
+    """ class that has a LANGUAGES class 
+    attribute equal to ["en", "fr"]."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-app.config.
-
+app.config.from_object(Config)
+babel = Babel()
 
 @app.route("/")
 def index():
